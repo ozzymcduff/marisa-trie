912a6c8864a0d42adcd8485aeae04c2879c35ff9
susumu.yata <susumu.yata@774d2c12-d1a0-b9a8-0dab-4f93c7fd7aa3>
2011-01-22 08:07:34 +0000

    Merged tests for IntVector and BitVector into vector-test.cc.

--- tests/unit-test.cc
+++ tests/unit-test.cc
@@ -480,5 +480,6 @@ int main() {
   TestQuery();
   TestContainer();
   TestCell();
+
   return 0;
 }
--- tests/vector-test.cc
+++ tests/vector-test.cc
@@ -1,12 +1,32 @@
-#include <cassert>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
 #include <vector>
 
 #include <marisa/vector.h>
+#include <marisa/intvector.h>
+#include <marisa/bitvector.h>
 
-int main() {
-  std::srand(static_cast<unsigned int>(time(NULL)));
+#define ASSERT(cond) { \
+  if (!(cond)) { \
+    std::cout << __LINE__ << ": " \
+        << "Assertion `" #cond "' failed." << std::endl; \
+    std::exit(-1); \
+  } \
+}
+
+#define EXCEPT(code, expected_status) try { \
+  code; \
+  std::cout << __LINE__ << ": " \
+      << "Exception `" #code "' failed." << std::endl; \
+  std::exit(-1); \
+} catch (const marisa::Exception &ex) { \
+  ASSERT(ex.status() == expected_status); \
+}
+
+void TestVector() {
+  std::cout << __FILE__ << ':' << __LINE__ << ": "
+      << __FUNCTION__ << "(): " << std::flush;
 
   std::vector<int> values;
   for (std::size_t i = 0; i < 1000; ++i) {
@@ -14,141 +34,286 @@ int main() {
   }
 
   marisa::Vector<int> vec;
-  assert(vec.max_size() ==
-      (MARISA_UINT32_MAX - sizeof(marisa::UInt32)) / sizeof(int));
 
-  assert(vec.size() == 0);
-  assert(vec.capacity() == 0);
-  assert(!vec.fixed());
-  assert(vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32));
+  ASSERT(vec.max_size() ==
+      (MARISA_UINT32_MAX - sizeof(marisa::UInt32)) / sizeof(int));
+  ASSERT(vec.size() == 0);
+  ASSERT(vec.capacity() == 0);
+  ASSERT(!vec.fixed());
+  ASSERT(vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32));
 
   for (std::size_t i = 0; i < values.size(); ++i) {
     vec.push_back(values[i]);
-    assert(vec[i] == values[i]);
+    ASSERT(vec[i] == values[i]);
+    ASSERT(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
   }
 
-  assert(vec.size() == values.size());
-  assert(vec.capacity() >= vec.size());
-  assert(!vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32)
+  ASSERT(vec.size() == values.size());
+  ASSERT(vec.capacity() >= vec.size());
+  ASSERT(!vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32)
       + ((sizeof(int) * values.size())));
 
-  assert(static_cast<const marisa::Vector<int> &>(vec).front()
+  ASSERT(static_cast<const marisa::Vector<int> &>(vec).front()
       == values.front());
-  assert(static_cast<const marisa::Vector<int> &>(vec).back()
+  ASSERT(static_cast<const marisa::Vector<int> &>(vec).back()
       == values.back());
-
-  assert(vec.front() == values.front());
-  assert(vec.back() == values.back());
+  ASSERT(vec.front() == values.front());
+  ASSERT(vec.back() == values.back());
 
   vec.shrink();
 
-  assert(vec.size() == values.size());
-  assert(vec.capacity() == vec.size());
-  assert(!vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32)
-      + ((sizeof(int) * values.size())));
-
+  ASSERT(vec.size() == values.size());
+  ASSERT(vec.capacity() == vec.size());
   for (std::size_t i = 0; i < values.size(); ++i) {
-    assert(vec[i] == values[i]);
-    assert(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
+    ASSERT(vec[i] == values[i]);
+    ASSERT(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
   }
 
   vec.save("vector-test.dat");
-
   vec.clear();
 
-  assert(vec.size() == 0);
-  assert(vec.capacity() == 0);
-  assert(vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32));
+  ASSERT(vec.empty());
+  ASSERT(vec.capacity() == 0);
 
   marisa::Mapper mapper;
   vec.mmap(&mapper, "vector-test.dat");
-  assert(mapper.is_open());
 
-  assert(vec.size() == values.size());
-  assert(vec.capacity() == 0);
-  assert(vec.fixed());
-  assert(!vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32)
+  ASSERT(mapper.is_open());
+  ASSERT(vec.size() == values.size());
+  ASSERT(vec.capacity() == 0);
+  ASSERT(vec.fixed());
+  ASSERT(!vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32)
       + ((sizeof(int) * values.size())));
 
   for (std::size_t i = 0; i < values.size(); ++i) {
-    assert(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
+    ASSERT(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
   }
 
   vec.clear();
   vec.load("vector-test.dat");
-  assert(vec.size() == values.size());
-  assert(vec.capacity() == vec.size());
-  assert(!vec.empty());
-  assert(vec.total_size() == sizeof(marisa::UInt32)
+
+  ASSERT(vec.size() == values.size());
+  ASSERT(vec.capacity() == vec.size());
+  ASSERT(!vec.fixed());
+  ASSERT(!vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32)
       + ((sizeof(int) * values.size())));
 
   for (std::size_t i = 0; i < values.size(); ++i) {
-    assert(vec[i] == values[i]);
-    assert(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
+    ASSERT(vec[i] == values[i]);
+    ASSERT(static_cast<const marisa::Vector<int> &>(vec)[i] == values[i]);
   }
 
-  try {
-    vec.resize(1U << 30);
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_SIZE_ERROR);
-  }
-  try {
-    vec.reserve(1U << 30);
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_SIZE_ERROR);
-  }
+  EXCEPT(vec.resize(1U << 30), MARISA_SIZE_ERROR);
+  EXCEPT(vec.reserve(1U << 30), MARISA_SIZE_ERROR);
 
   vec.clear();
 
   vec.push_back(0);
-  assert(vec.capacity() == 1);
+  ASSERT(vec.capacity() == 1);
   vec.push_back(1);
-  assert(vec.capacity() == 2);
+  ASSERT(vec.capacity() == 2);
   vec.push_back(2);
-  assert(vec.capacity() == 4);
-  vec.push_back(3);
-  assert(vec.capacity() == 4);
-  vec.push_back(4);
-  assert(vec.capacity() == 8);
-  vec.resize(13);
-  assert(vec.capacity() == 16);
+  ASSERT(vec.capacity() == 4);
+  vec.resize(5);
+  ASSERT(vec.capacity() == 8);
   vec.resize(100);
-  assert(vec.capacity() == 100);
+  ASSERT(vec.capacity() == 100);
 
-  assert(!vec.fixed());
   vec.fix();
-  assert(vec.fixed());
-  try {
-    vec.fix();
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_STATE_ERROR);
+  ASSERT(vec.fixed());
+  EXCEPT(vec.fix(), MARISA_STATE_ERROR);
+  EXCEPT(vec.push_back(0), MARISA_STATE_ERROR);
+  EXCEPT(vec.resize(0), MARISA_STATE_ERROR);
+  EXCEPT(vec.reserve(0), MARISA_STATE_ERROR);
+
+  std::cout << "ok" << std::endl;
+}
+
+void TestIntVector() {
+  std::cout << __FILE__ << ':' << __LINE__ << ": "
+      << __FUNCTION__ << "(): " << std::flush;
+
+  marisa::IntVector vec;
+
+  ASSERT(vec.num_bits_per_int() == 0);
+  ASSERT(vec.mask() == 0);
+  ASSERT(vec.size() == 0);
+  ASSERT(vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32) * 4);
+
+  marisa::Vector<marisa::UInt32> values;
+  vec.build(values);
+
+  ASSERT(vec.num_bits_per_int() == 1);
+  ASSERT(vec.mask() == 1);
+  ASSERT(vec.size() == 0);
+  ASSERT(vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32) * 4);
+
+  values.push_back(0);
+  vec.build(values);
+
+  ASSERT(vec.num_bits_per_int() == 1);
+  ASSERT(vec.mask() == 1);
+  ASSERT(vec.size() == 1);
+  ASSERT(!vec.empty());
+  ASSERT(vec.total_size() == sizeof(marisa::UInt32) * 5);
+  ASSERT(vec[0] == 0);
+
+  values.push_back(255);
+  vec.build(values);
+
+  ASSERT(vec.num_bits_per_int() == 8);
+  ASSERT(vec.mask() == 0xFF);
+  ASSERT(vec.size() == 2);
+  ASSERT(vec[0] == 0);
+  ASSERT(vec[1] == 255);
+
+  values.push_back(65536);
+  vec.build(values);
+
+  ASSERT(vec.num_bits_per_int() == 17);
+  ASSERT(vec.mask() == 0x1FFFF);
+  ASSERT(vec.size() == 3);
+  ASSERT(vec[0] == 0);
+  ASSERT(vec[1] == 255);
+  ASSERT(vec[2] == 65536);
+
+  vec.save("vector-test.dat");
+  vec.clear();
+
+  ASSERT(vec.num_bits_per_int() == 0);
+  ASSERT(vec.mask() == 0);
+  ASSERT(vec.size() == 0);
+
+  marisa::Mapper mapper;
+  vec.mmap(&mapper, "vector-test.dat");
+
+  ASSERT(mapper.is_open());
+  ASSERT(vec.num_bits_per_int() == 17);
+  ASSERT(vec.mask() == 0x1FFFF);
+  ASSERT(vec.size() == 3);
+  ASSERT(vec[0] == 0);
+  ASSERT(vec[1] == 255);
+  ASSERT(vec[2] == 65536);
+
+  vec.clear();
+  vec.load("vector-test.dat");
+
+  ASSERT(vec.num_bits_per_int() == 17);
+  ASSERT(vec.mask() == 0x1FFFF);
+  ASSERT(vec.size() == 3);
+  ASSERT(vec[0] == 0);
+  ASSERT(vec[1] == 255);
+  ASSERT(vec[2] == 65536);
+
+  values.clear();
+  for (std::size_t i = 0; i < 500; ++i) {
+    values.push_back(std::rand());
   }
-  try {
-    vec.push_back(0);
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_STATE_ERROR);
+  vec.build(values);
+
+  ASSERT(vec.size() == values.size());
+  for (std::size_t i = 0; i < vec.size(); ++i) {
+    ASSERT(vec[i] == values[i]);
   }
-  try {
-    vec.resize(0);
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_STATE_ERROR);
+
+  EXCEPT(vec.build(MARISA_UINT32_MAX, MARISA_UINT32_MAX - 3),
+      MARISA_SIZE_ERROR);
+
+  std::cout << "ok" << std::endl;
+}
+
+void TestBitVector(marisa::UInt32 size) {
+  marisa::BitVector bv;
+
+  ASSERT(bv.size() == 0);
+  ASSERT(bv.empty());
+  ASSERT(bv.total_size() == sizeof(marisa::UInt32) * 5);
+
+  std::vector<bool> bits(size);
+  std::vector<marisa::UInt32> zeros, ones;
+  for (marisa::UInt32 i = 0; i < size; ++i) {
+    const bool bit = (std::rand() % 2) == 0;
+    bits[i] = bit;
+    bv.push_back(bit);
+    (bit ? ones : zeros).push_back(i);
+    ASSERT(bv[i] == bits[i]);
   }
-  try {
-    vec.reserve(0);
-    assert(false);
-  } catch (const marisa::Exception &ex) {
-    assert(ex.status() == MARISA_STATE_ERROR);
+
+  ASSERT(bv.size() == bits.size());
+  ASSERT((size == 0) || !bv.empty());
+
+  bv.build();
+
+  marisa::UInt32 num_zeros = 0, num_ones = 0;
+  for (marisa::UInt32 i = 0; i < bits.size(); ++i) {
+    ASSERT(bv[i] == bits[i]);
+    ASSERT(bv.rank0(i) == num_zeros);
+    ASSERT(bv.rank1(i) == num_ones);
+    ++(bv[i] ? num_ones : num_zeros);
   }
+  for (marisa::UInt32 i = 0; i < zeros.size(); ++i) {
+    ASSERT(bv.select0(i) == zeros[i]);
+  }
+  for (marisa::UInt32 i = 0; i < ones.size(); ++i) {
+    ASSERT(bv.select1(i) == ones[i]);
+  }
+
+  std::stringstream stream;
+  bv.write(stream);
+  bv.clear();
+
+  ASSERT(bv.size() == 0);
+  ASSERT(bv.empty());
+  ASSERT(bv.total_size() == sizeof(marisa::UInt32) * 5);
+
+  bv.read(stream);
+
+  ASSERT(bv.size() == bits.size());
+
+  num_zeros = 0, num_ones = 0;
+  for (marisa::UInt32 i = 0; i < bits.size(); ++i) {
+    ASSERT(bv[i] == bits[i]);
+    ASSERT(bv.rank0(i) == num_zeros);
+    ASSERT(bv.rank1(i) == num_ones);
+    ++(bv[i] ? num_ones : num_zeros);
+  }
+  for (std::size_t i = 0; i < zeros.size(); ++i) {
+    ASSERT(bv.select0(i) == zeros[i]);
+  }
+  for (std::size_t i = 0; i < ones.size(); ++i) {
+    ASSERT(bv.select1(i) == ones[i]);
+  }
+}
+
+void TestBitVector() {
+  std::cout << __FILE__ << ':' << __LINE__ << ": "
+      << __FUNCTION__ << "(): " << std::flush;
+
+  TestBitVector(0);
+  TestBitVector(1);
+  TestBitVector(511);
+  TestBitVector(512);
+  TestBitVector(513);
+
+  for (marisa::UInt32 i = 0; i < 100; ++i) {
+    TestBitVector(std::rand() % 4096);
+  }
+
+  std::cout << "ok" << std::endl;
+}
+
+int main() {
+  std::srand((unsigned int)time(NULL));
+
+  TestVector();
+  TestIntVector();
+  TestBitVector();
 
   return 0;
 }
