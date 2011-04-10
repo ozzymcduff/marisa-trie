df8d90b02a112f297ffc4e3b422be181d93423a4
susumu.yata <susumu.yata@774d2c12-d1a0-b9a8-0dab-4f93c7fd7aa3>
2011-04-10 01:29:34 +0000

    Update: marisa-0.2.0-beta.

---
+++ branches/marisa-0.2.0-beta/lib/marisa/grimoire/vector/precomputed-table.h
@@ -0,0 +1,25 @@
+#ifndef MARISA_GRIMOIRE_VECTOR_PRECOMPUTED_TABLE_H_
+#define MARISA_GRIMOIRE_VECTOR_PRECOMPUTED_TABLE_H_
+
+#include "../../base.h"
+
+namespace marisa {
+namespace grimoire {
+namespace vector {
+
+class PrecomputedTable {
+ public:
+  static std::size_t select(std::size_t i, UInt8 byte) {
+    MARISA_DEBUG_IF(i > 7, MARISA_BOUND_ERROR);
+    return select_table_[i][byte];
+  }
+
+ private:
+  static const UInt8 select_table_[8][256];
+};
+
+}  // namespace marisa
+}  // namespace grimoire
+}  // namespace vector
+
+#endif  // MARISA_GRIMOIRE_VECTOR_PRECOMPUTED_TABLE_H_
